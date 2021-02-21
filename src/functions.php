4e94aadd81a4fcbdc55b75e76127224288da960f
Osvaldo Jiang <osvaldo@publitar.com>
2021-02-21 11:27:46 -0300

    Update

--- src/functions.php
+++ src/functions.php
@@ -1,21 +1,45 @@
 <?php
+namespace {
+    use Choval\Email;
 
-use Choval\Email;
+    function email_parse(string $email)
+    {
+        $obj = new Email($email);
+        return $obj->parse();
+    }
 
-function email_parse(string $email)
-{
-    $obj = new Email($email);
-    return $obj->parse();
+    function email_safe(string $email)
+    {
+        $obj = new Email($email);
+        return $obj->safe();
+    }
+
+    function email_valid(string $email)
+    {
+        $obj = new Email($email);
+        return $obj->valid();
+    }
+
 }
+namespace Choval\Email {
+    use Choval\Email;
 
-function email_safe(string $email)
-{
-    $obj = new Email($email);
-    return $obj->safe();
-}
+    function email_parse(string $email)
+    {
+        $obj = new Email($email);
+        return $obj->parse();
+    }
 
-function email_valid(string $email)
-{
-    $obj = new Email($email);
-    return $obj->valid();
+    function email_safe(string $email)
+    {
+        $obj = new Email($email);
+        return $obj->safe();
+    }
+
+    function email_valid(string $email)
+    {
+        $obj = new Email($email);
+        return $obj->valid();
+    }
+
 }
